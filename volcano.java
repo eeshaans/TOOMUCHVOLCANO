05bfa3f4532641cca79fbd6d444432771ae46d51
eeshaans <88553981+eeshaans@users.noreply.github.com>
2022-01-18 14:40:21 -0500

    Update volcano.java

--- volcano.java
+++ volcano.java
@@ -7,27 +7,16 @@
  */
 public class volcano
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class volcano
-     */
-    public volcano()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
+    int height; 
+    int age;
+    int temperature;
+    byte VEI;
+    String name;
+    
+    
+    
+   public volcano(){
+       
+   }
 }
